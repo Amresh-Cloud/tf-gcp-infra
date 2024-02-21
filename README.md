fe36df681540bf3a728f834266df631a3e3f049b
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-02-21 18:59:04 -0500

    Added Firewalls,enabled  HTTP, VM

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # tf-gcp-infra
 
-# API Enabled
- ## Compute Engine API
+- API Enabled
 
-# added Yml
+- Compute Engine API
+
+- Added Yml
+
+- Added Firewals
+
+- Added SSH Blocking,
+
+-  Enabled HTTP
+
+-  Added VM Instance
+
 
 
--- main.tf
+++ main.tf
@@ -24,3 +24,47 @@ resource "google_compute_route" "webapp_internet_route" {
   network          = google_compute_network.amresh.id
   next_hop_gateway = var.next_hop_gateway_default
 }
+resource "google_compute_firewall" "blocking_ssh" {
+  name = var.block_ssh
+  network = google_compute_network.amresh.name
+  deny {
+    protocol = var.protocol
+    ports= var.disable_port
+  }
+  source_ranges = var.source_ranges
+}
+resource "google_compute_firewall" "enabled_http" {
+  name = var.enabled_http
+  network = google_compute_network.amresh.name
+
+  allow {
+    protocol = var.protocol
+    ports = var.port_allowed
+  }
+
+  source_ranges = var.source_ranges
+  target_tags = var.target_tags
+}
+resource "google_compute_instance" "webapp_vm" {
+  name          = var.webapp_VM_Name
+  machine_type  = var.machinetype
+  zone          = var.zone
+  tags          = var.tags
+     boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.disksize
+      type  = var.disktype
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+      // Add access configuration if necessary
+    }
+  }
+  service_account {
+    email  = var.service_email
+    scopes = var.scope
+  }
+}
